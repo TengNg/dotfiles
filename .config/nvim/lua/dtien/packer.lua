2f54c071ff8f91faf9d29c84bd393ce668755ffe
TengNg <nguyentin148@gmail.com>
2022-10-10 16:55:26 +0700

    adding nvim-treesitter & nvim-luasnip

--- .config/nvim/lua/dtien/packer.lua
+++ .config/nvim/lua/dtien/packer.lua
@@ -1,20 +1,23 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
+use 'wbthomason/packer.nvim'
     use 'folke/tokyonight.nvim'
+
     use 'nvim-lualine/lualine.nvim'
+    use 'kyazdani42/nvim-web-devicons'
+
     use 'preservim/nerdtree'
 
-    use 'nvim-telescope/telescope.nvim'
-
     -- All the things
-    use 'neovim/nvim-lspconfig' -- LSP
-    use 'hrsh7th/nvim-cmp'      -- Completion
-    use 'nvim-lua/plenary.nvim' -- Common utilities
-    use 'onsails/lspkind-nvim'  -- vscode-like pictograms
-    use 'hrsh7th/cmp-buffer'    -- nvim-cmp source for buffer words
-    use 'hrsh7th/cmp-nvim-lsp'  -- nvim-cmp source for neovim's built-in LSP
+    use 'neovim/nvim-lspconfig'         -- LSP
+    use 'hrsh7th/nvim-cmp'              -- Completion
+    use 'nvim-telescope/telescope.nvim' -- Fzf
+    use 'nvim-lua/plenary.nvim'         -- Common utilities
+    use 'onsails/lspkind-nvim'          -- vscode-like pictograms
+    use 'hrsh7th/cmp-buffer'            -- nvim-cmp source for buffer words
+    use 'hrsh7th/cmp-nvim-lsp'          -- nvim-cmp source for neovim's built-in LSP
+    use 'L3MON4D3/LuaSnip'
 
     use {
         "nvim-treesitter/nvim-treesitter",
