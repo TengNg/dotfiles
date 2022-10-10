a4c98a6d1bd05d4923ebd8755679308406af8d30
TengNg <nguyentin148@gmail.com>
2022-10-10 08:53:42 +0700

    updated

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,15 +1,1 @@
-require("dtien.init")
-
-local has = vim.fn.has
-local is_mac = has "macunix"
-local is_win = has "win32"
-
-if is_mac then
-  require('dtien.macos')
-end
-if is_win then
-  require('dtien.windows')
-end
-
-vim.cmd [[highlight Normal ctermbg=none]]
-vim.cmd [[highlight NonText ctermbg=none]]
+require("dtien")
--- .config/nvim/lua/dtien/init.lua
+++ .config/nvim/lua/dtien/init.lua
@@ -2,6 +2,20 @@ require("dtien.set")
 require("dtien.remaps")
 require("dtien.packer")
 
+-------------------------------------------------
+
+local has = vim.fn.has
+local is_mac = has "macunix"
+local is_win = has "win32"
+if is_mac then
+  require('dtien.macos')
+end
+if is_win then
+  require('dtien.windows')
+end
+
+-------------------------------------------------
+
 local augroup = vim.api.nvim_create_augroup
 MyGroup = augroup('', {})
 
