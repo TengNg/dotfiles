b334e96c636eca330be9d9b9312c40231592c975
TengNg <nguyentin148@gmail.com>
2022-12-22 11:22:11 +0700

    updated

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,10 +1,18 @@
-local scheme = 'rose-pine'
-function set_scheme(choice)
-	color = color or scheme
-	vim.cmd.colorscheme(color)
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    vim.api.nvim_set_hl(0, 'SignColumn', { bg = "none" })
-end
+local status, color = pcall(require, "rose-pine")
+if (not status) then return end
 
-set_scheme()
+color.setup({
+    disable_background = true
+})
+
+vim.cmd.colorscheme('rose-pine')
+
+-- function set_scheme(choice)
+--     color = color or scheme
+--     vim.cmd.colorscheme(color)
+--     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--     vim.api.nvim_set_hl(0, 'SignColumn', { bg = "none" })
+-- end
+--
+-- set_scheme()
